ff9554075d6409c467b9434986f45aab28d0a260
kimguan-breeze <chukimguan@breeze.cash>
2026-01-07 13:35:22 +0800

    feat: url

--- components/product-page.tsx
+++ components/product-page.tsx
@@ -162,7 +162,7 @@ function PaymentSection() {
           {isIframe ? (
             <iframe
               id="breeze-payment-page"
-              src="https://pay2.qa.breeze.cash/page_37c19d5ed7d01967/pcs_ec4ad83d44282b6a573d01431e1d4665e0bc884d/desktop"
+              src="https://pay2.breeze.cash/page_21f64c8e09e96908/pcs_d91dbf63c93b96d8a17b792541dee3aab30322fd/android"
               // src={`https://pay.qa.breeze.cash/${paymentPage.pageId}/${paymentPage.clientSecret}`}
               // src="https://api.breeze.cash/v1/invoices/client/invc_551b067467b8a141"
               className="w-full h-full"
